10e5ab343e5c1ba13ac507b115d2775c407e1697
Jon Griggs <bigmac_beats1773@outlook.com>
2023-11-07 22:07:42 -0600

    Added movement functionality

--- pacman.js
+++ pacman.js
@@ -18,11 +18,17 @@ const layout = [
 ]
 
 let map = document.querySelector("#world")
+let pacIcon = document.querySelector("#pacman")
+let pacman = {
+    x: 164,
+    y: 157
+}
+const step = 24 //todo pixels
 
 const displayWorld = () => {
     let output = ''
     for (let i = 0; i < layout.length; i++) {
-        output += "<div class='row'>"
+        output += "\n<div class='row'>\n"
         for (let j = 0; j < layout[i].length; j++) {
             if (layout[i][j] == 2) {
                 output += "\n\t<div class='brick'></div>"
@@ -34,11 +40,44 @@ const displayWorld = () => {
                 output += "\n\t<div class='empty'></div>"
             }
         }
-        output += "\n</div>\n"
+        output += "</div>"
     }
-    console.log(output)
     map.innerHTML = output
+}
+
+const displayPacman = () => {
+    pacIcon.style.top = pacman.y + "px"
+    pacIcon.style.left = pacman.x + "px"
 }
 
 
 displayWorld()
+displayPacman()
+
+//? Key Codes
+    //* Left Arrow = 37
+    //* Right Arrow = 39
+    //* Up Arrow = 38
+    //* Down Arrow = 40
+
+
+
+document.onkeydown = (e) => {
+    console.log(e.keyCode)
+    if (e.keyCode == 37) {
+        pacman.x -= 24
+    }
+    if (e.keyCode == 39) {
+        pacman.x += 24
+    }
+    if (e.keyCode == 38) {
+        pacman.y -= 24
+    }
+    if (e.keyCode == 40) {
+        pacman.y += 24
+    }
+    displayPacman()
+}
+
+
+
